15f304fdc6b5c9f96ebfc6119a495f75e9cb0fc5
jaimebaik <jaimebaik5@gmail.com>
2021-02-16 12:07:26 -0800

    [backend/add]a query for the vendor table to fetch all data

--- backend/lib/graphql/schema.py
+++ backend/lib/graphql/schema.py
@@ -2,14 +2,21 @@ import graphene
 from graphene import ObjectType, String, Schema
 from graphene_sqlalchemy import SQLAlchemyObjectType
 from models.user_model import UserModel
+from models.vendor_model import VendorModel
 
 class User(SQLAlchemyObjectType):
   class Meta:
     model = UserModel
 
+class Vendor(SQLAlchemyObjectType):
+  class Meta:
+    model = VendorModel
+
 class Query(ObjectType):
   users = graphene.List(User)
   user = graphene.Field(User, id=graphene.Int())
+
+  vendors = graphene.List(Vendor)
 
   def resolve_users(root, info):
     query = User.get_query(info)  # SQLAlchemy query
@@ -19,4 +26,9 @@ class Query(ObjectType):
     query = User.get_query(info)
     return query.filter(UserModel.id == id).first()
 
+  def resolve_vendors(root, info): 
+    query = Vendor.get_query(info)  # SQLAlchemy query
+    return query.all()
+  
 schema = Schema(query=Query)
+
